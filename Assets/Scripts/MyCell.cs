f9e36018607b114d23e83e079b69079086686fa9
CodeLim <hockpeng_lim@yahoo.com.sg>
2018-08-05 14:16:21 +0800

    Initial commit

---
+++ Assets/Scripts/MyCell.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyCell : MonoBehaviour
+{
+    public Text mLabel;
+    public Button mButton;
+    public Main mMain;
+
+    public void Fill()
+    {
+        mButton.interactable = false;
+
+        // Set childed text
+        mLabel.text = mMain.GetTurnCharacter();
+
+        // Switch turns
+        mMain.Switch();
+    }
+}
